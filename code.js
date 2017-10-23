7c300c6f50c01f9bb1c033a31b6dbc396ca1d848
poitras <nicopoitras@gmail.com>
2017-10-22 23:44:06 -0400

    First iteration date working

---
+++ code.js
@@ -0,0 +1,6 @@
+/**
+ * 1. Write a JavaScript program to display the current day and time in the following format.  Go to the editor
+ * Sample Output : Today is : Friday. 
+ * Current time is : 4 PM : 50 : 22
+ */
+console.log('Hello world');
